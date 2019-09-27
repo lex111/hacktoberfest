266dcf3cbdd492645f317d23f1b4248f7be886f2
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-09-27 18:24:19 -0400

    deploy

---
+++ precache-manifest.49f73aea7de985de9ee023cde8ef559d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8145b347dcb07a5dcc84943841969104",
+    "url": "/img/Hacktoberfest_19_Events_lockup_ko_300x100.8145b347.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5c1991280c4bab842ccd9f2cc75294e7",
+    "url": "/img/hacktober19_swag-mockup.5c199128.png"
+  },
+  {
+    "revision": "2aea0428f6aa04727ec8",
+    "url": "/js/app.3984ae62.js"
+  },
+  {
+    "revision": "51db5d1085d145fea529",
+    "url": "/js/chunk-vendors.be1dfefc.js"
+  },
+  {
+    "revision": "c05bf42e59793d8c15cc",
+    "url": "/js/about.102de71e.js"
+  },
+  {
+    "revision": "5ef32c08640e7b15efcabfcc2fa9213d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60af15b836f87c9944a252083238be22",
+    "url": "/img/snyk-logo.60af15b8.png"
+  },
+  {
+    "revision": "e987798c8d88c41c3a46788e6573d2cf",
+    "url": "/img/pulumi-logo.e987798c.png"
+  },
+  {
+    "revision": "4f2bfde4babb28995b39e444f2d32dde",
+    "url": "/img/orbtoberfest-header_graphic.4f2bfde4.svg"
+  },
+  {
+    "revision": "fd48c4065198e6c99542ae6918352843",
+    "url": "/img/circleci-logo.fd48c406.png"
+  },
+  {
+    "revision": "59142f9de60e921813b4d7168999224c",
+    "url": "/img/circle-logo-horizontal-white.59142f9d.svg"
+  },
+  {
+    "revision": "51db5d1085d145fea529",
+    "url": "/css/chunk-vendors.2b10fbaa.css"
+  },
+  {
+    "revision": "2aea0428f6aa04727ec8",
+    "url": "/css/app.0a88b0cc.css"
+  }
+];
